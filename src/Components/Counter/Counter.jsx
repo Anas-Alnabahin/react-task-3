1e7e58a48164cb4e42ef2b530696b63559bbed48
Anas-Alnabahin <anasnb2002nb@gmail.com>
2022-12-22 12:01:33 +0200

    final adjustments

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 export default class Counter extends Component {
   render() {
     return (
-      <div style={{ padding: 20 }}>
+      <div style={{ padding: 20 }} className="container">
+        <p>{this.props.count}</p>
         <button
+          className="decrement"
           onClick={() => this.props.onDecrement(this.props.id, this.props.step)}
         >
           -
         </button>
-        <p>{this.props.count}</p>
         <button
+          className="increment"
           onClick={() => this.props.onIncrement(this.props.id, this.props.step)}
         >
           +
